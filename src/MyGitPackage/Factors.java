e0903037e97b8022aae57a0ee893bd59adcf20dd
Sonali Jadhav <sonali.jadhav@tatanexarc.com>
2025-01-06 12:57:38 +0530

    My fisrt commit in branch#

--- src/MyGitPackage/Factors.java
+++ src/MyGitPackage/Factors.java
@@ -1,5 +1,19 @@
 package MyGitPackage;
 
+import java.util.Scanner;
+
 public class Factors {
+	public static void main(String args[])
+	{
+		int fact = 1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int x=sc.nextInt();
+		for(int i=x; i>0;i--)
+		{
+		  fact=fact*i;
+		}
+		System.out.println("Factorial of number: "+fact);
+	}
 
 }
